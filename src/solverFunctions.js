75d27975c780b57aaae24dd42ace8623131db7ea
Mike <mweinberg21@gmail.com>
2018-01-31 15:13:08 -0600

    Deleted old isCellSubfocused function and tests

--- src/solverFunctions.js
+++ src/solverFunctions.js
@@ -49,39 +49,3 @@ export const updatePencilMarksForCellHelper = (data, pencilMarks, squareId) => {
 }
 export const updatePencilMarksForCell = (data, pencilMarks, squareId) =>
   global.store.dispatch(dataActions.setPencilMarks(updatePencilMarksForCellHelper(data, pencilMarks, squareId)))
-
-
-
-// Slated for deletion
-export const isCellSubfocued = (focusCell, focusType, squareId) => {
-  if (focusType === '' || focusCell === squareId) {
-    return false
-  }
-
-  if (focusType === 'row') {
-    const squareRow = squareId.charAt(0)
-    const focusRow = focusCell.charAt(0)
-
-    return squareRow === focusRow
-  } else if (focusType === 'col') {
-    const squareCol = squareId.charAt(2)
-    const focusCol = focusCell.charAt(2)
-
-    return squareCol === focusCol
-  } else if (focusType === 'box') {
-    const squareRow = squareId.charAt(0)
-    const focusRow = focusCell.charAt(0)
-    const squareCol = squareId.charAt(2)
-    const focusCol = focusCell.charAt(2)
-
-    if (Math.abs(squareRow - focusRow) > 2 || Math.abs(squareCol - focusCol) > 2) {
-      return false
-    }
-
-    const cellsInBox = cellsSharingBox(focusCell)
-
-    return cellsInBox.includes(squareId)
-  }
-
-  return false
-}
