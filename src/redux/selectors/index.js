9b983a55dff0c70a6fb075113057d61d07503518
Mike <mweinberg21@gmail.com>
2018-01-31 11:20:01 -0600

    Created subfocusedCells selector and tests

---
+++ src/redux/selectors/index.js
@@ -0,0 +1,2 @@
+export const getFocusCell = state => state.app.focusCell
+export const getFocusType = state => state.app.focusType
---
+++ test/selectors/subfocusedCells.spec.js
@@ -0,0 +1,87 @@
+import subfocusedCells from '../../src/redux/selectors/subfocusedCells'
+
+describe('isCellSubfocued function tests', () => {
+  it('Returns empty array if focusType is empty string', () => {
+    const mockState = {
+      app: {
+        focusCell: '1-1',
+        focusType: '',
+      }
+    }
+
+    const actual = subfocusedCells(mockState)
+    const expected = []
+    
+    expect(actual).toEqual(expected)
+  })
+
+  it('Returns focused cells for row of 5-2', () => {
+    const mockState = {
+      app: {
+        focusCell: '5-2',
+        focusType: 'row',
+      }
+    }
+
+    const actual = subfocusedCells(mockState)
+    const expected = [
+      '5-1',
+      '5-3',
+      '5-4',
+      '5-5',
+      '5-6',
+      '5-7',
+      '5-8',
+      '5-9',
+    ]
+    
+    expect(actual).toEqual(expected)
+  })
+
+  it('Returns focused cells for col of 7-3', () => {
+    const mockState = {
+      app: {
+        focusCell: '7-3',
+        focusType: 'col',
+      }
+    }
+
+    const actual = subfocusedCells(mockState)
+    const expected = [
+      '1-3',
+      '2-3',
+      '3-3',
+      '4-3',
+      '5-3',
+      '6-3',
+      '8-3',
+      '9-3',
+    ]
+    
+    expect(actual).toEqual(expected)
+  })
+
+
+  it('Returns focused cells for box of 1-3', () => {
+    const mockState = {
+      app: {
+        focusCell: '1-3',
+        focusType: 'box',
+      }
+    }
+
+    const actual = subfocusedCells(mockState)
+    const expected = [
+      '1-1',
+      '2-1',
+      '3-1',
+      '1-2',
+      '2-2',
+      '3-2',
+      '2-3',
+      '3-3',
+    ]
+    
+    expect(actual).toEqual(expected)
+  })
+})
