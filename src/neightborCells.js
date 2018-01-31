3ac3e1eb8e63ccc84a0e40bfbc5f3d3c2cebdff1
Mike <mweinberg21@gmail.com>
2018-01-31 00:55:28 -0600

    Function and test for neighbor cells column

--- src/neightborCells.js
+++ src/neightborCells.js
@@ -17,7 +17,20 @@ export const cellsSharingRow = cell => {
 
 
 export const cellsSharingCol = cell => {
-  // TODO Write test
+  const col = cell.charAt(2)
+  const sharedColCells = [
+    `1-${col}`,
+    `2-${col}`,
+    `3-${col}`,
+    `4-${col}`,
+    `5-${col}`,
+    `6-${col}`,
+    `7-${col}`,
+    `8-${col}`,
+    `9-${col}`,
+  ]
+
+  return sharedColCells.filter(id => id !== cell)
 }
 
 
--- test/neighborCells.spec.js
+++ test/neighborCells.spec.js
@@ -55,6 +55,56 @@ describe('Neighbor Cells Functions', () => {
   })
 
 
+  it('Returns cells sharing the col of 7-2', () => {
+    const cellId = '7-2'
+    const actual = cellsSharingCol(cellId)
+    const expected = [
+      '1-2',
+      '2-2',
+      '3-2',
+      '4-2',
+      '5-2',
+      '6-2',
+      '8-2',
+      '9-2',
+    ]
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('Returns cells sharing the col of 7-8', () => {
+    const cellId = '7-8'
+    const actual = cellsSharingCol(cellId)
+    const expected = [
+      '1-8',
+      '2-8',
+      '3-8',
+      '4-8',
+      '5-8',
+      '6-8',
+      '8-8',
+      '9-8',
+    ]
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('Returns cells sharing the col of 2-9', () => {
+    const cellId = '2-9'
+    const actual = cellsSharingCol(cellId)
+    const expected = [
+      '1-9',
+      '3-9',
+      '4-9',
+      '5-9',
+      '6-9',
+      '7-9',
+      '8-9',
+      '9-9',
+    ]
+
+    expect(actual).toEqual(expected)
+  })
 
 
 
