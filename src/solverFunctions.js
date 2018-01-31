582e9677bbe193517d6c10ebdfe5016e0bff3fe3
Mike <mweinberg21@gmail.com>
2018-01-30 21:30:52 -0600

    Wrote test for changeCellInData

--- src/solverFunctions.js
+++ src/solverFunctions.js
@@ -1,13 +1,9 @@
 export const changeCellInData = (data, squareId, oldValue, newValue) => {
-
-  // TODO Write test for this!!!
-
   const valueNum = newValue === '' ? 0 : parseInt(newValue)
 
   if (isNaN(valueNum)) {
     return data
   }
-
   const inRangeValue = newValue < 0 || newValue > 9 ? oldValue : valueNum
 
   return {
