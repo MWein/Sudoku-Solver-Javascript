e9a761112f023f0d62372daf9b039de60db97546
Mike <mweinberg21@gmail.com>
2018-01-30 22:12:12 -0600

    Added controls to AppContainer and attached enable and disable actions to start button

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,14 +1,58 @@
+import Button from 'material-ui/Button'
+import PropTypes from 'prop-types'
 import React from 'react'
 import SudokuGrid from '../components/SudokuGrid'
+import { actions as appActions } from '../redux/actions/appActions'
+import { connect } from 'react-redux'
 
-const AppContainer = () => (
+const divStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+}
+
+const AppContainer = ({
+  enabled,
+  setEnabled,
+  setDisabled,
+}) => (
   <div>
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
 
+    <div style={divStyle}>
+      
+      <Button
+        onClick={enabled ? () => setDisabled() : () => setEnabled()}
+      >
+        {enabled ? 'Start' : 'Stop'}
+      </Button>
+
+      <Button
+        disabled={!enabled}
+      >
+        Step
+      </Button>
+    </div>
+
+    <br />
+
+    <div style={divStyle}>
       <SudokuGrid />
-
     </div>
   </div>
 )
 
-export default AppContainer
+
+AppContainer.propTypes = {
+  enabled: PropTypes.bool.isRequired,
+  setDisabled: PropTypes.func.isRequired,
+  setEnabled: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  enabled: state.app.enabled,
+})
+
+const actions = {
+  ...appActions,
+}
+
+export default connect(mapStateToProps, actions)(AppContainer)
