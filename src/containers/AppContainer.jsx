feefb967c09c7177de1bb40ea39fe1156cabad4c
Mike <mweinberg21@gmail.com>
2018-01-31 20:02:39 -0600

    Added a few temporary controls for focusCell and focusType

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,7 +1,9 @@
 import Button from 'material-ui/Button'
+import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SudokuGrid from '../components/SudokuGrid'
+import TextField from 'material-ui/TextField'
 import { actions as appActions } from '../redux/actions/appActions'
 import { connect } from 'react-redux'
 
@@ -14,6 +16,10 @@ const AppContainer = ({
   enabled,
   setEnabled,
   setDisabled,
+  focusCell,
+  focusType,
+  setFocusCell,
+  setFocusType,
 }) => (
   <div>
 
@@ -29,25 +35,45 @@ const AppContainer = ({
       >
         Step
       </Button>
+
+
+      <TextField
+        onChange={input => setFocusCell(input.target.value)}
+        style={{ width: '50px' }}
+        value={focusCell}
+      />
+      <TextField
+        onChange={input => setFocusType(input.target.value)}
+        style={{ width: '50px' }}
+        value={focusType}
+      />
     </div>
 
     <br />
 
+
     <div style={divStyle}>
       <SudokuGrid />
     </div>
+
   </div>
 )
 
 
 AppContainer.propTypes = {
   enabled: PropTypes.bool.isRequired,
+  focusCell: PropTypes.string,
+  focusType: PropTypes.string,
   setDisabled: PropTypes.func.isRequired,
   setEnabled: PropTypes.func.isRequired,
+  setFocusCell: PropTypes.func.isRequired,
+  setFocusType: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
   enabled: state.app.enabled,
+  focusCell: state.app.focusCell,
+  focusType: state.app.focusType,
 })
 
 const actions = {
