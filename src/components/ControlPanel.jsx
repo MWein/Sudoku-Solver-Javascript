a2bacba78b14ac8044a474e29b987ef312d9ee39
Mike <mweinberg21@gmail.com>
2018-02-01 23:53:44 -0600

    I know, I'm using a table. I'm disgusted too

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,28 +1,51 @@
+import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { actions as appActions } from '../redux/actions/appActions'
 import { connect } from 'react-redux'
 
 
-const ControlPanel = () => {
+const ControlPanel = ({
+  enabled,
+  setEnabled,
+  setDisabled,
+}) => {
 
   const linterAway = 'Hello, World'
 
   return (
-    <Paper style={calcPaperStyle()}>
+    <Paper>
       {linterAway}
+
+
+      <Button
+        onClick={enabled ? () => setDisabled() : () => setEnabled()}
+      >
+        {enabled ? 'Start' : 'Stop'}
+      </Button>
+      <Button
+        disabled={!enabled}
+      >
+        Step
+      </Button>
     </Paper>
   )
 }
 
 
 ControlPanel.propTypes = {
+  enabled: PropTypes.bool.isRequired,
+  setDisabled: PropTypes.func.isRequired,
+  setEnabled: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
+  enabled: state.app.enabled,
 })
 
 const actions = {
+  ...appActions,
 }
 
 export default connect(mapStateToProps, actions)(ControlPanel)
--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,82 +1,28 @@
-import Button from 'material-ui/Button'
-import PropTypes from 'prop-types'
+import ControlPanel from '../components/ControlPanel'
 import React from 'react'
 import SudokuGrid from '../components/SudokuGrid'
-import TextField from 'material-ui/TextField'
-import { actions as appActions } from '../redux/actions/appActions'
-import { connect } from 'react-redux'
 
 const divStyle = {
   display: 'flex',
   justifyContent: 'center',
 }
 
-const AppContainer = ({
-  enabled,
-  setEnabled,
-  setDisabled,
-  focusCell,
-  focusType,
-  setFocusCell,
-  setFocusType,
-}) => (
-  <div>
-
-    <div style={divStyle}>
-      <Button
-        onClick={enabled ? () => setDisabled() : () => setEnabled()}
-      >
-        {enabled ? 'Start' : 'Stop'}
-      </Button>
-
-      <Button
-        disabled={!enabled}
-      >
-        Step
-      </Button>
-
-
-      <TextField
-        onChange={input => setFocusCell(input.target.value)}
-        style={{ width: '50px' }}
-        value={focusCell}
-      />
-      <TextField
-        onChange={input => setFocusType(input.target.value)}
-        style={{ width: '50px' }}
-        value={focusType}
-      />
-    </div>
-
-    <br />
-
-
-    <div style={divStyle}>
-      <SudokuGrid />
-    </div>
-
+const AppContainer = () => (
+  <div style={divStyle}>
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <SudokuGrid />
+          </td>
+          <td width='50px' />
+          <td>
+            <ControlPanel />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 )
 
-
-AppContainer.propTypes = {
-  enabled: PropTypes.bool.isRequired,
-  focusCell: PropTypes.string,
-  focusType: PropTypes.string,
-  setDisabled: PropTypes.func.isRequired,
-  setEnabled: PropTypes.func.isRequired,
-  setFocusCell: PropTypes.func.isRequired,
-  setFocusType: PropTypes.func.isRequired,
-}
-
-const mapStateToProps = state => ({
-  enabled: state.app.enabled,
-  focusCell: state.app.focusCell,
-  focusType: state.app.focusType,
-})
-
-const actions = {
-  ...appActions,
-}
-
-export default connect(mapStateToProps, actions)(AppContainer)
+export default AppContainer
