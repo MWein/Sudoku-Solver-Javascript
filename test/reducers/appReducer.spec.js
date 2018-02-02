f5c6f4d49a445772b1afd451d81b02fd5f3a13a6
Mike <mweinberg21@gmail.com>
2018-02-02 01:14:48 -0600

    Reorganized appReducer test

--- test/reducers/appReducer.spec.js
+++ test/reducers/appReducer.spec.js
@@ -16,6 +16,26 @@ describe('App reducer spec', () => {
     expect(actual).toEqual(initialState)
   })
 
+  it('Responds to SET_ENABLED', () => {
+    const action = {
+      type: appActions.SET_ENABLED,
+      payload: null,
+    }
+
+    const mockState = {
+      ...initialState,
+      enabled: false,
+    }
+
+    const expected = {
+      ...initialState,
+      enabled: true,
+    }
+
+    const actual = reducer(mockState, action)
+    expect(actual).toEqual(expected)
+  })
+
   it('Responds to SET_DISABLED', () => {
     const action = {
       type: appActions.SET_DISABLED,
@@ -43,26 +63,6 @@ describe('App reducer spec', () => {
       puzzleState: action.payload
     }
 
-    expect(actual).toEqual(expected)
-  })
-
-  it('Responds to SET_ENABLED', () => {
-    const action = {
-      type: appActions.SET_ENABLED,
-      payload: null,
-    }
-
-    const mockState = {
-      ...initialState,
-      enabled: false,
-    }
-
-    const expected = {
-      ...initialState,
-      enabled: true,
-    }
-
-    const actual = reducer(mockState, action)
     expect(actual).toEqual(expected)
   })
 
