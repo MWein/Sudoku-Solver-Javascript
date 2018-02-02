5cc0c7f6ce60eb93aa543a268401a75628f99c23
Mike <mweinberg21@gmail.com>
2018-02-02 00:07:24 -0600

    ControlPanel creates sections dynamically

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,34 +1,59 @@
 import Button from 'material-ui/Button'
+import Divider from 'material-ui/Divider'
 import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Typography from 'material-ui/Typography'
 import { actions as appActions } from '../redux/actions/appActions'
 import { connect } from 'react-redux'
-
 
 const ControlPanel = ({
   enabled,
   setEnabled,
   setDisabled,
 }) => {
+  const paperPadding = '10px'
+  const paddingStyle = {
+    paddingTop: paperPadding,
+    paddingBottom: paperPadding,
+    paddingLeft: paperPadding,
+    paddingRight: paperPadding,
+  }
 
-  const linterAway = 'Hello, World'
+  const createSection = (title, content, hasDivider) => (
+    <div style={paddingStyle}>
+      <Typography type='title'>{title}</Typography>
+      <br />
+      {content}
+      {hasDivider ? <div><br /><Divider /></div> : null}
+    </div>
+  )
 
   return (
-    <Paper>
-      {linterAway}
+    <Paper style={paddingStyle}>
+
+      {
+        createSection(
+          'Solver',
+          (
+            <div>
+              <Button
+                onClick={enabled ? () => setDisabled() : () => setEnabled()}
+              >
+                {enabled ? 'Start' : 'Stop'}
+              </Button>
+              <Button
+                disabled={!enabled}
+              >
+                Step
+              </Button>
+            </div>
+          ),
+          false
+        )
+      }
 
 
-      <Button
-        onClick={enabled ? () => setDisabled() : () => setEnabled()}
-      >
-        {enabled ? 'Start' : 'Stop'}
-      </Button>
-      <Button
-        disabled={!enabled}
-      >
-        Step
-      </Button>
     </Paper>
   )
 }
